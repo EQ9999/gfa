a1793650c6e30513d6f6208dc0ec3a0653fcbfb5
EQ9999 <1287400541@qq.com>
2021-02-19 16:12:01 +0800

    update

--- src/main/java/indi/hdy/share/gfa/Test.java
+++ src/main/java/indi/hdy/share/gfa/Test.java
@@ -1,5 +1,26 @@
 package indi.hdy.share.gfa;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class Test {
 
+	@Value("${thello}")
+	private String test;
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return test;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test.class);
+	}
 }
