1aeb5724c6e7d39ba4fdbfc94b1b299f653e28e3
hdy <hdy@candeMacBook-Air.local>
2021-02-19 20:37:39 +0800

    add gateway config

--- src/main/java/indi/hdy/share/gfa/Test.java
+++ src/main/java/indi/hdy/share/gfa/Test.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,14 @@ public class Test {
 	public String hello() {
 		return test;
 	}
+	
+//	@Bean
+//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route("path_route", r -> r.path("/s")
+//						.uri("https://www.baidu.com"))
+//				.build();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test.class);
